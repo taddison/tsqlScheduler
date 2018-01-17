7f6b606e0de0b7647eeb60be074934ff7354a9c6
petervandivier <petervandivier@gmail.com>
2018-01-17 15:35:12 +0000

    simplify patchfile & correct default name on create-table scripts

--- deploy/patchfiles/1.5/add-NotifyLevel-column-and-default.sql
+++ deploy/patchfiles/1.5/add-NotifyLevel-column-and-default.sql
@@ -1,21 +1,9 @@
 if col_length(N'scheduler.Task',N'NotifyLevelEventlog') is null
-    alter table scheduler.Task add NotifyLevelEventlog int not null default 2
+    alter table scheduler.Task add NotifyLevelEventlog int not null constraint DF_NotifyLevelEventlog default 2
 go
 
-if exists( select * 
-            from sys.default_constraints dc
-            where dc.parent_object_id = object_id(N'scheduler.Task',N'U')
-                and dc.parent_column_id = try_convert(int,columnproperty(object_id(N'scheduler.Task',N'U'),'NotifyLevelEventlog','ColumnId')))
-begin; -- default exists, drop & re-create to ensure accuracy
-    declare @dSql nvarchar(max);
-
-    select @dSql='alter table scheduler.Task drop constraint ['+dc.[name]+'];'
-    from sys.default_constraints dc
-    where dc.parent_object_id = object_id(N'scheduler.Task',N'U')
-        and dc.parent_column_id = columnproperty(object_id(N'scheduler.Task',N'U'),'NotifyLevelEventlog','ColumnId');
-
-    exec sys.sp_executesql @dSql;
-end;
+alter table scheduler.Task drop constraint DF_NotifyLevelEventlog;
+go
 
 alter table scheduler.Task add constraint DF_Task_NotifyLevelEventlog default (2) for NotifyLevelEventlog;
 go
